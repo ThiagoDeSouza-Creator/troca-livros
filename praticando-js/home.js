2cde6f0e965de8d4f057ee5499b6feb04d4a4a76
Thiago De Souza Silva <thiago007002@gmail.com>
2026-08-21 21:16:29 -0300

    Praticando JS

--- praticando-js/home.js
+++ praticando-js/home.js
@@ -34,3 +34,32 @@ btnAmarelo.addEventListener( 'click', () => {
 btnAzul.addEventListener( 'click', () => {
     caixa.style.backgroundColor = "blue";
 });
+
+ /// Simulando adição e subtração de valor ///
+const valorReal = document.getElementById("valor_real");
+let contador = 0;
+
+document.getElementById("btn_menos").addEventListener( 'click', () => {
+    if( contador > 0){
+    contador --;
+    valorReal.textContent = contador;
+    }
+});
+
+document.getElementById("btn_mais").addEventListener( 'click', () => {
+    contador ++;
+    valorReal.textContent = contador;
+});
+
+// Adicionando Livros //
+
+const nomeLivro = document.getElementById("nome_livro");
+const lista_livros = document.getElementById("lista_livros");
+ 
+document.getElementById("adiciona_livro").addEventListener('click',
+     () => {
+        const novoLivro = document.createElement("li")
+        novoLivro.textContent = nomeLivro.value;
+        lista_livros.appendChild(novoLivro)
+ 
+     });
